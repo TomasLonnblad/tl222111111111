c0c6b10488bf6f6ab843b4a0a9d8621f53c22d1c
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-23 15:10:02 +0100

    x

--- components/aurora-background-demo.tsx
+++ components/aurora-background-demo.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
+
+import { motion } from "motion/react";
 import React from "react";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 
