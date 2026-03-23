c0ee9005cd581b1be03e31a925f0a66fba4f7eff
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-23 13:56:56 +0100

    x

---
+++ components/aurora-background-demo.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { motion } from "motion/react";
+import React from "react";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+
+export default function AuroraBackgroundDemo() {
+  return (
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 8, y: 180 }}
+        whileInView={{ opacity: 2, y: 10 }}
+        transition={{
+          delay: 0.3,
+          duration: 2.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          Background lights are cool you know.
+        </div>
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+          And this, is chemical burn.
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          Debug now
+        </button>
+      </motion.div>
+    </AuroraBackground>
+  );
+}
